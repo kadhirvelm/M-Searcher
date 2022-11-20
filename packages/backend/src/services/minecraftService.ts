85cdedb2259fb2478ca9e4ef00adc4a4cc0a1b36
Kadhir Manickam <kadhirvelm@gmail.com>
2022-11-20 10:50:26 -0800

    Add minecraft package

--- packages/backend/src/services/minecraftService.ts
+++ packages/backend/src/services/minecraftService.ts
@@ -1,9 +1,13 @@
 import { IMinecraftSearchService } from "@minecraft/api";
+import minecraft from "minecraft-data";
+
+const MINECRAFT_DATA = minecraft("1.19");
 
 export function searchRecipes(
     payload: IMinecraftSearchService["searchRecipes"]["payload"],
 ): Promise<IMinecraftSearchService["searchRecipes"]["response"]> {
-    console.log(payload);
+    console.log(MINECRAFT_DATA.recipes[1]);
+
     return new Promise((resolve) => {
         resolve(payload);
     });
