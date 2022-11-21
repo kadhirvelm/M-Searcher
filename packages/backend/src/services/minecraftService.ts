d40980e7386cf6abbf47d02a8edc3bb1ece6efa2
Kadhir Manickam <kadhirvelm@gmail.com>
2022-11-20 18:06:41 -0800

    remove a thing

--- packages/backend/src/services/minecraftService.ts
+++ packages/backend/src/services/minecraftService.ts
@@ -1,6 +1,5 @@
 import { IMinecraftSearchService } from "@minecraft/api";
 import minecraft from "minecraft-data";
-import icon_mapping from "./icon_map.json";
 
 const MINECRAFT_DATA = minecraft("1.19");
 
@@ -8,8 +7,6 @@ export function searchRecipes(
   payload: IMinecraftSearchService["searchRecipes"]["payload"],
 ): Promise<IMinecraftSearchService["searchRecipes"]["response"]> {
   console.log(MINECRAFT_DATA.itemsByName.feather, payload.searchText);
-
-  console.log(icon_mapping[0]);
 
   return new Promise((resolve) => {
     resolve(payload.searchText);
